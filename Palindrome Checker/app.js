3ccc96d1b1d60251a2f4572608fb773d4d96123a
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-11 06:46:35 +0300

    palindrome Checker

--- Palindrome Checker/app.js
+++ Palindrome Checker/app.js
@@ -1,5 +1,5 @@
 function palindrome(str) {
-    // delete regex from str and shang letter to lower case 
+    // delete regex from str and shang letter to lower case.
     let a = str.replace(/[`~ !@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,"").toLowerCase();
     // if str = reverse str  true 
      return a === a.split("").reverse().join("")
